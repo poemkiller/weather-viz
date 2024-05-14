b9be38a7d05b0e797d58f2f6228c8500bf15a2a3
poemkilller <licam665@newschool.edu>
2024-05-14 19:34:09 -0400

    finalpush

--- src/components/FiveDayForecast.jsx
+++ src/components/FiveDayForecast.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 
 function FiveDayForecast({ forecast }) {
@@ -7,18 +7,31 @@ function FiveDayForecast({ forecast }) {
   }
 
   return (
-    <div>
-      {forecast.map((day, index) => (
-        <div key={index}>
-          <p>
-            {" "}
-            {new Date(day.dt_txt).toLocaleDateString("en-US", {
-              weekday: "short",
-            })}
-          </p>
-          <p>{day.main.temp.toFixed()}°F</p>
-        </div>
-      ))}
+    <div className="window scale-down" style={{ width: "30rem", marginTop: "20px" }}>
+      <div className="title-bar">
+        <h1 className="title">Five Day Forecast</h1>
+        <button aria-label="Close" className="close"></button>
+        <button aria-label="Resize" disabled className="hidden"></button>
+      </div>
+      <div className="separator"></div>
+
+      <div className="modeless-dialog" style={{ padding: "10px" }}>
+        {forecast.map((day, index) => (
+          <div key={index} className="field-row" style={{ justifyContent: "space-between", margin: "10px 0", backgroundColor: "#f0f0f0", borderRadius: "20px", padding: "10px 20px", color: "black" }}>
+            <p style={{ margin: 0 }}>
+              {new Date(day.dt_txt).toLocaleDateString("en-US", { weekday: "short" })}
+            </p>
+            <div style={{ textAlign: "right" }}>
+              <p style={{ margin: 0 }}>
+                High: {day.main.temp_max.toFixed()}°F
+              </p>
+              <p style={{ margin: 0 }}>
+                Low: {day.main.temp_min.toFixed()}°F
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }

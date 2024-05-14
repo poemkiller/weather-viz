d4c7bfd7e18649d61ea3371d3b934198a3c516a1
poemkilller <licam665@newschool.edu>
2024-05-14 19:55:42 -0400

    Initit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'build',  // Specifies the output directory for build files
+  }
+});
